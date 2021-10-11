002d748a1d760a9871a89f06cdd79705a72f8e58
AntonSerbin <antons.zn@gmail.com>
2021-10-11 18:40:14 +0300

    HW2

--- HomeWork2/index.php
+++ HomeWork2/index.php
@@ -8,16 +8,32 @@
 //создать переменную массив [‘alex’, ‘vova’, ‘tolya’, [‘kostya’, ‘olya’, [‘gosha’, mila]]] вести ее на экран
 //создать переменную массив [[‘alex’, ‘vova’, ‘tolya’], [‘kostya’, ‘olya’], [‘gosha’, mila]] вести ее на экран
 
-
 $name="Anton Serbin";
+echo $name. "\n";
 print "$name". "\n";
+print_r($name);
+echo "\n---------------\n";
+
 $date = 100500;
-print "age=$date\n";
+echo $date. "\n";
+print "$date\n";
+print_r($date);
+echo "\n---------------\n";
+
+
 $dataPi=M_PI;
+echo $dataPi. "\n";
 print $dataPi."\n";
+print_r($dataPi);
+echo "\n---------------\n";
+
 $names1=['alex', 'vova', 'tolya'];
 $names2=['alex', 'vova', 'tolya', ['kostya', 'olya']];
 $names3=['alex', 'vova', 'tolya', ['kostya', 'olya'], ['gosha', 'mila']];
 $names4=[['alex', 'vova', 'tolya'], ['kostya', 'olya'], ['gosha', 'mila']];
-for ($i = 1; $i <= 4; $i++) print_r(${"names".$i});
+for ($i = 1; $i <= 4; $i++) {
+//    echo "\n" .${"names".$i}. "\n";
+//    print ${"names".$i}. "\n";
+    print_r(${"names".$i});
+};
 ?>
