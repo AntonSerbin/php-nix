787ca0e84e51c14ae9882b01bfc724b6ba732904
AntonSerbin <antons.zn@gmail.com>
2021-10-11 18:44:46 +0300

    HW2

--- HomeWork2/index.php
+++ HomeWork2/index.php
@@ -10,30 +10,16 @@
 
 $name="Anton Serbin";
 echo $name. "\n";
-print "$name". "\n";
-print_r($name);
-echo "\n---------------\n";
 
 $date = 100500;
-echo $date. "\n";
 print "$date\n";
-print_r($date);
-echo "\n---------------\n";
-
 
 $dataPi=M_PI;
 echo $dataPi. "\n";
-print $dataPi."\n";
-print_r($dataPi);
-echo "\n---------------\n";
 
 $names1=['alex', 'vova', 'tolya'];
 $names2=['alex', 'vova', 'tolya', ['kostya', 'olya']];
 $names3=['alex', 'vova', 'tolya', ['kostya', 'olya'], ['gosha', 'mila']];
 $names4=[['alex', 'vova', 'tolya'], ['kostya', 'olya'], ['gosha', 'mila']];
-for ($i = 1; $i <= 4; $i++) {
-//    echo "\n" .${"names".$i}. "\n";
-//    print ${"names".$i}. "\n";
-    print_r(${"names".$i});
-};
+for ($i = 1; $i <= 4; $i++) print_r(${"names".$i});
 ?>
