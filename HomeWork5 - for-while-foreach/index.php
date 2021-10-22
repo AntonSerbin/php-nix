e421f5524d621cfdae74716f01dcc6ceec5eec6c
AntonSerbin <antons.zn@gmail.com>
2021-10-22 09:09:15 +0300

    A lot of letters

---
+++ HomeWork5 - for-while-foreach/index.php
@@ -0,0 +1,23 @@
+<?php
+if ($_POST) {
+    echo "<br>";
+    $quizNumber = rand(5,8);
+    echo "Было загадано число $quizNumber, а вы указали ".$_POST['numberForm'].". <br>";
+
+    if ($_POST["numberForm"] < 5) echo "число маленькое<br>";
+    elseif ($_POST["numberForm"] > 8) echo "число БОЛЬШОЕ<br>";
+
+    elseif ($_POST["numberForm"] == $quizNumber) { ?>
+        <?= "УРА! Вы угадали!<br>"; ?>
+        <br>
+        <a href="form.php">Играть еще!</a>
+    <?php
+    }
+    elseif ($_POST["numberForm"]>= 5 && $_POST["numberForm"] <= 8){ ?>
+        <?= "Почти угадали! <br>"; ?>
+        <br>
+        <a href="form.php">Попробуйте еще!</a>
+    <?php
+    };
+}
+?>
