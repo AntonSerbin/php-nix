8e6887c90683d80a6ab1ffe6e304f2919b240a5e
AntonSerbin <antons.zn@gmail.com>
2021-10-22 08:59:41 +0300

    Home work4, a lot of letters

---
+++ HomeWork4/form.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form</title>
+</head>
+
+<form action="index.php"  method="post">
+    <label for="numberForm">Enter number</label>
+    <input name="numberForm" type="number" pattern="\d*" min="1" max="100">
+    <input type="submit" value="УГАДАТЬ">
+
+</form>
+
+</form>
+
+<body>
+
+</body>
+</html>
--- HomeWork4/index.php
+++ HomeWork4/index.php
@@ -1,62 +1,23 @@
-<!----------------Task1------------------->
-<!--создать форму оставить отзыв-->
-<!--поле имя-->
-<!--поле комментарий-->
+<?php
+if ($_POST) {
+    echo "<br>";
+    $quizNumber = rand(5,8);
+    echo "Было загадано число $quizNumber, а вы указали ".$_POST['numberForm'].". <br>";
 
-<label for="reviewForm">The form of review, please enter your name and review</label>
-<form name = "reviewForm" action="serv.php" method="post">
-    <label for="review">Review: >
-        <textarea name="review"></textarea>
-    </label>
-    <br>
-    <label for="name">Name:>
-        <input type="text" name="name">
-    </label>
-    <br>
-    <button type="submit">OK this form</button>
-    <button type="reset">RESET this form</button>
+    if ($_POST["numberForm"] < 5) echo "число маленькое<br>";
+    elseif ($_POST["numberForm"] > 8) echo "число БОЛЬШОЕ<br>";
 
-</form>
-
-<!----------------Task2------------------->
-<!--создать форму добавить товар в корзину-->
-<!--id товара-->
-<!--количество-->
-<br>
-<label for="cartID">The cart. Please add ID and quantity</label>
-<form name = "cartID" action="serv.php" method="post">
-    <label for="goodID">ID number: >
-        <input type="number" name="goodID"  >
-    </label>
-    <br>
-    <label for="quantityGood">Quantity: >
-        <input type="number" name="quantityGood" min="0" max="9999">
-    </label>
-    <br>
-    <button type="submit">OK this form</button>
-    <button type="reset">RESET this form</button>
-
-</form>
-
-<!----------------Task3------------------->
-<!--обратная связь перезвонить клиенту-->
-<!--имя-->
-<!--телефон-->
-
-<br>
-<label for="callbackForm">Callback form</label>
-<form name = "callbackForm" action="serv.php" method="post">
-    <label for="nameCallback">Name: >
-        <input type="text" name="nameCallback"  >
-    </label>
-    <br>
-    <label for="phoneCallback">Phone: >
-        <input type="tel" name="phoneCallback" pattern="[+]{1}[0-9]{12}"><br>
-        <small>Format: +380997168362</small>
-    </label>
-    <br>
-    <button type="submit">OK this form</button>
-    <button type="reset">RESET this form</button>
-
-</form>
-
+    elseif ($_POST["numberForm"] == $quizNumber) { ?>
+        <?= "УРА! Вы угадали!<br>"; ?>
+        <br>
+        <a href="form.php">Играть еще!</a>
+    <?php
+    }
+    elseif ($_POST["numberForm"]>= 5 && $_POST["numberForm"] <= 8){ ?>
+        <?= "Почти угадали! <br>"; ?>
+        <br>
+        <a href="form.php">Попробуйте еще!</a>
+    <?php
+    };
+}
+?>
